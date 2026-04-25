17eb13a110ce0afcd7dc149f3b4b535c5e57f2d3
Subham Mondal <subhamoffi@gmail.com>
2026-04-25 22:42:16 +0530

    Changed the path for loading state in global layout and also added a trycatch for start and stop loading

---
+++ app/( HOME )/register/layout.tsx
@@ -0,0 +1,7 @@
+export default function RegisterLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="text-black flex items-center justify-center min-h-screen">
+            {children}
+        </div>
+    );
+}
---
+++ app/( HOME )/register/page.tsx
@@ -0,0 +1,119 @@
+"use client";
+import { userValidation } from "@/_lib/validation";
+import { useLoadingStore } from "@/stores/loading-store";
+import { useRouter } from "next/navigation";
+import { useRef, useState } from "react";
+
+const Page = () => {
+    const router = useRouter();
+    const startLoading = useLoadingStore((state) => state.start);
+    const stopLoading = useLoadingStore((state) => state.end);
+    const passwordElmRef = useRef<HTMLInputElement>(null);
+
+    const [userDetails, setUserDetails] = useState({
+        name: "",
+        email: "",
+        password: "",
+    });
+
+    const handleOnchange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUserDetails({
+            ...userDetails,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleRegister = async () => {
+        const { success } = userValidation.safeParse(userDetails);
+        if (!success) return alert("Please enter valid details");
+        startLoading();
+        try {
+            const res = await fetch("/api/auth/register", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(userDetails),
+            });
+            if (res.ok) {
+                stopLoading();
+                router.push("/dashboard");
+                return;
+            }
+            const err = await res.json();
+            alert(err.message);
+        } catch (error) {
+            alert("somethign went wrong");
+        } finally {
+            stopLoading();
+        }
+    };
+
+    return (
+        <div className="w-90 rounded-2xl shadow-[0_3px_10px_rgb(0,0,0,0.2)] p-4 flex flex-col gap-5">
+            <h1 className="text-center font-bold text-3xl">Register</h1>
+            <div className="flex gap-2 flex-col">
+                <div className="flex flex-col">
+                    <label htmlFor="name">Name</label>
+                    <input
+                        type="text"
+                        className="border rounded-lg h-8 border-neutral-400 px-2"
+                        placeholder="John Doe"
+                        name="name"
+                        value={userDetails.name}
+                        onChange={handleOnchange}
+                    />
+                </div>
+                <div className="flex flex-col">
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="text"
+                        className="border rounded-lg h-8 border-neutral-400 px-2"
+                        placeholder="johndoe@gmail.com"
+                        name="email"
+                        value={userDetails.email}
+                        onChange={handleOnchange}
+                    />
+                </div>
+                <div className="flex flex-col">
+                    <label htmlFor="password">Password</label>
+                    <div className="flex gap-2 items-center">
+                        <input
+                            type="password"
+                            className="w-full border rounded-lg h-8 border-neutral-400 px-2"
+                            name="password"
+                            value={userDetails.password}
+                            onChange={handleOnchange}
+                            ref={passwordElmRef}
+                        />
+                        <input
+                            type="checkbox"
+                            className="peer appearance-none h-5 w-5 border border-black rounded-md checked:bg-blue-500"
+                            onClick={() => {
+                                if (passwordElmRef.current) {
+                                    if (passwordElmRef.current?.type == "password")
+                                        passwordElmRef.current.type = "text";
+                                    else passwordElmRef.current.type = "password";
+                                }
+                            }}
+                        />
+                    </div>
+                </div>
+            </div>
+            <button
+                className="bg-black text-white h-10 rounded-lg hover:bg-neutral-900 cursor-pointer"
+                onClick={handleRegister}
+            >
+                Submit
+            </button>
+            <span className="text-center text-sm">
+                Already have an account?{" "}
+                <a href="/login" className="hover:underline ">
+                    Login
+                </a>
+            </span>
+        </div>
+    );
+};
+
+export default Page;
