21b82393ed6b9a74b2fe9c2b613fbfa4d6b36064
Subham Mondal <subhamoffi@gmail.com>
2026-04-26 15:56:09 +0530

    Created the v1 of navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,16 @@
+import { useProductStore } from "@/stores/product-store";
+
 const Navbar = () => {
+    const showProduct = useProductStore((state) => state.showAddProduct);
     return (
         <div className="sticky top-2 h-12 backdrop-blur-3xl flex items-center justify-center gap-10 w-fit mx-auto px-10 mt-2 rounded-2xl border border-neutral-600 font-heading">
             <div className="hover:underline cursor-pointer transition-discrete duration-300">
                 Home
             </div>
-            <div className="hover:underline cursor-pointer transition-discrete duration-300">
+            <div
+                className="hover:underline cursor-pointer transition-discrete duration-300"
+                onClick={showProduct}
+            >
                 Add Product
             </div>
             <div className="hover:underline cursor-pointer transition-discrete duration-300">
