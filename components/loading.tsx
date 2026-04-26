2f6806cd1be8860f420cc99a6ae497f8a02ca27f
Subham Mondal <subhamoffi@gmail.com>
2026-04-26 15:55:48 +0530

    Created the workign loading component

--- components/loading.tsx
+++ components/loading.tsx
@@ -1,5 +1,13 @@
+import Image from "next/image";
+
 const Loading = () => {
-    return <div className="z-10 absolute inset-0 bg-gray-500">Loading</div>;
+    return (
+        <div className="z-10 fixed inset-0 flex items-center justify-center bg-white">
+            <div className="w-20 h-20 relative">
+                <Image alt="" src="/loading.gif" width={100} height={100} />
+            </div>
+        </div>
+    );
 };
 
 export default Loading;
