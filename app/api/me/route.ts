0dacd46aab274cee3df752ba14976153494f8e2d
Subham Mondal <subhamoffi@gmail.com>
2026-05-04 14:16:35 +0530

    Clean the get user by server actions

--- app/api/me/route.ts
+++ app/api/me/route.ts
@@ -1,16 +1,9 @@
-import { verify } from "jsonwebtoken";
-import { cookies } from "next/headers";
-import { NextResponse } from "next/server";
-
-const JWT_SECRET = process.env.JWT_SECRET || "";
+import { getUserWithRoomStatus } from "@/actions/user";
 
 export async function GET() {
-    const token = (await cookies()).get("token")?.value;
-    if (!token) return NextResponse.json({ user: null });
-    try {
-        const decoded = verify(token, JWT_SECRET);
-        return NextResponse.json({ user: decoded });
-    } catch {
-        return NextResponse.json({ user: null });
-    }
+    //Get user from server actions
+    const res = await getUserWithRoomStatus();
+    if (!res) return Response.json({ user: null, roomId: null });
+    const data = await res.json();
+    return Response.json(data);
 }
