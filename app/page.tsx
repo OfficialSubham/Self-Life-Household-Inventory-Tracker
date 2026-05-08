8b17ef76d22e12209861b6920efda3f4c04a915c
Subham Mondal <subhamoffi@gmail.com>
2026-05-08 15:21:17 +0530

    Implemented the two components for join and create home

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import CreateHome from "@/components/createHome";
+import JoinHome from "@/components/joinHome";
+import { useState } from "react";
+
+const Page = () => {
+    const [isCreateHome, setIsCreateHome] = useState(true);
+    return (
+        <div className="min-h-screen flex items-center justify-center flex-col">
+            {isCreateHome ? (
+                <CreateHome setIsCreateHome={setIsCreateHome} />
+            ) : (
+                <JoinHome setIsCreateHome={setIsCreateHome} />
+            )}
+        </div>
+    );
+};
+
+export default Page;
