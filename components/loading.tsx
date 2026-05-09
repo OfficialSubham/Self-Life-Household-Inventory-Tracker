ef7854cbcdfb2192b62941199380d0544073588d
Subham Mondal <subhamoffi@gmail.com>
2026-05-09 21:16:27 +0530

    Updated the loading file to show even in login page

--- components/loading.tsx
+++ components/loading.tsx
@@ -1,12 +1,32 @@
+"use client";
+
+import { useLoadingStore } from "@/stores/loading-store";
 import Image from "next/image";
+import { useEffect } from "react";
 
 const Loading = () => {
+    const loading = useLoadingStore((state) => state.loading);
+
+    useEffect(() => {
+        if (loading) {
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflow = "auto";
+        }
+        console.log("Running Use Effect");
+        return () => {
+            document.body.style.overflow = "auto";
+        };
+    }, [loading]);
+
     return (
-        <div className="z-10 fixed inset-0 flex items-center justify-center bg-white">
-            <div className="w-20 h-20 relative">
-                <Image alt="" src="/loading.gif" width={100} height={100} />
+        loading && (
+            <div className="z-10 fixed inset-0 flex items-center justify-center bg-white">
+                <div className="w-20 h-20 relative">
+                    <Image alt="" src="/loading.gif" width={100} height={100} />
+                </div>
             </div>
-        </div>
+        )
     );
 };
 
