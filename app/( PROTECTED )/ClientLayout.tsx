3a02f175872c1f77457a7b935c5cc385643bd2bd
Subham Mondal <subhamoffi@gmail.com>
2026-05-23 19:24:27 +0530

    Taking user and all products and setting up it in the store

---
+++ app/( PROTECTED )/ClientLayout.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { useEffect } from "react";
+import { useUserStore } from "@/stores/user-store";
+import { ProductDetails } from "@/lib/types";
+import { useProductStore } from "@/stores/product-store";
+
+export default function ClientLayout({
+    user,
+    items,
+    children,
+    houseDetails,
+}: {
+    children: React.ReactNode;
+    user: {
+        name: string;
+        email: string;
+        _id: number;
+        householdId: number | null;
+    };
+    items: ProductDetails[];
+    houseDetails: {
+        _id: number;
+        name: string;
+        inviteCode: string;
+        wasteScore: number | null;
+        ownerId: number;
+    } | null;
+}) {
+    const setUser = useUserStore((state) => state.setUser);
+    const setIsOwner = useUserStore((state) => state.setIsOwner);
+    const setProducts = useProductStore((state) => state.setProducts);
+    useEffect(() => {
+        if (!setUser) return;
+        setUser(user);
+        if (houseDetails) {
+            setIsOwner(houseDetails.ownerId == user._id);
+        }
+    }, [user, setUser, setIsOwner, houseDetails]);
+
+    useEffect(() => {
+        setProducts(items);
+    }, [items, setProducts]);
+    return <>{children}</>;
+}
