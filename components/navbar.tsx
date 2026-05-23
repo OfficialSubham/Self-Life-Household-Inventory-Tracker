4688b8fd5f38fa22704aa02f51a1ca510e98d468
Subham Mondal <subhamoffi@gmail.com>
2026-05-23 19:27:27 +0530

    Created Navbar component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useRouter } from "next/navigation";
 
 const contents = ["Home", "About", "Profile", "Members"];
@@ -6,20 +7,22 @@ const Navbar = () => {
     const router = useRouter();
 
     return (
-        <div className="sticky top-2 h-12 backdrop-blur-3xl flex items-center justify-center gap-10 w-fit mx-auto px-10 mt-2 rounded-2xl border border-neutral-600 font-heading">
-            {contents.map((navLink, idx) => {
-                return (
-                    <div
-                        className="hover:underline cursor-pointer transition-discrete duration-300"
-                        key={idx}
-                        onClick={() =>
-                            router.push(navLink[0].toLowerCase() + navLink.slice(1))
-                        }
-                    >
-                        {navLink}
-                    </div>
-                );
-            })}
+        <div className="z-100 fixed top-2 flex items-center justify-center w-full">
+            <div className="flex gap-10 w-fit h-12 items-center px-10 mt-2 rounded-2xl border backdrop-blur-3xl border-neutral-600 font-heading ">
+                {contents.map((navLink, idx) => {
+                    return (
+                        <div
+                            className="hover:underline cursor-pointer transition-discrete duration-300"
+                            key={idx}
+                            onClick={() =>
+                                router.push(navLink[0].toLowerCase() + navLink.slice(1))
+                            }
+                        >
+                            {navLink}
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     );
 };
