f5a58513fc795e6c66d5f5b1b121160fbc6c75da
Subham Mondal <subhamoffi@gmail.com>
2026-05-24 21:29:55 +0530

    Implemented necessary product functions like sort edit delete reset

--- stores/product-store.ts
+++ stores/product-store.ts
@@ -3,7 +3,10 @@ import { create } from "zustand";
 
 export type ProductState = {
     addProductVisible: boolean;
+    editProductVisible: boolean;
+    editProductDetails: ProductDetails | null;
     allProducts: ProductDetails[] | null;
+    displayedProducts: ProductDetails[] | null;
 };
 
 export type ProductStoreActions = {
@@ -11,6 +14,13 @@ export type ProductStoreActions = {
     removeAddProduct: () => void;
     setProducts: (products: ProductDetails[]) => void;
     addProduct: (product: ProductDetails) => void;
+    toggleEditProduct: (toggle: boolean) => void;
+    setEditProductDetails: (product: ProductDetails) => void;
+    editTheProduct: (id: number, product: ProductDetails) => void;
+    deleteTheProduct: (id: number) => void;
+    sortProductsByExpiry: () => void;
+    resetProduct: () => void;
+    sortLexicographical: () => void;
 };
 
 export type ProductStore = ProductState & ProductStoreActions;
@@ -18,11 +28,57 @@ export type ProductStore = ProductState & ProductStoreActions;
 export const useProductStore = create<ProductStore>((set) => ({
     addProductVisible: false,
     allProducts: null,
+    displayedProducts: null,
+    editProductVisible: false,
+    editProductDetails: null,
     showAddProduct: () => set({ addProductVisible: true }),
     removeAddProduct: () => set({ addProductVisible: false }),
-    setProducts: (products) => set({ allProducts: products }),
+    setProducts: (products) =>
+        set({ allProducts: products, displayedProducts: products }),
     addProduct: (product) =>
         set((state) => ({
             allProducts: state.allProducts?.concat(product),
+            displayedProducts: state.allProducts?.concat(product),
         })),
+    toggleEditProduct: (toggle) => set({ editProductVisible: toggle }),
+    setEditProductDetails: (product) => set({ editProductDetails: product }),
+    editTheProduct: (id, product) =>
+        set((state) => ({
+            allProducts: state.allProducts?.map((eachProd) => {
+                if (eachProd._id == id) return product;
+                return eachProd;
+            }),
+            displayedProducts: state.allProducts,
+        })),
+    deleteTheProduct: (id) =>
+        set((state) => ({
+            allProducts: state.allProducts?.filter((eachProd) => eachProd._id != id),
+            displayedProducts: state.allProducts,
+        })),
+    sortProductsByExpiry: () =>
+        set((state) => {
+            if (!state.allProducts || !state.displayedProducts)
+                return { allProducts: [] };
+
+            return {
+                displayedProducts: [...state.displayedProducts].sort((a, b) => {
+                    return (
+                        new Date(a.expiryDate!).getTime() -
+                        new Date(b.expiryDate!).getTime()
+                    );
+                }),
+            };
+        }),
+    resetProduct: () => set((state) => ({ displayedProducts: state.allProducts })),
+    sortLexicographical: () =>
+        set((state) => {
+            if (!state.allProducts || !state.displayedProducts)
+                return { allProducts: [] };
+
+            return {
+                displayedProducts: [...state.displayedProducts].sort((a, b) => {
+                    return a.name.localeCompare(b.name);
+                }),
+            };
+        }),
 }));
