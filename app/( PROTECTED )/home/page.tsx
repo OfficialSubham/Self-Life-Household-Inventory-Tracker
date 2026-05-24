09d6dac84d3397d043d6fa7d9d1e8a3a9ae2f95f
Subham Mondal <subhamoffi@gmail.com>
2026-05-24 21:28:10 +0530

    Change from all product to display product for keeping the original

--- app/( PROTECTED )/home/page.tsx
+++ app/( PROTECTED )/home/page.tsx
@@ -3,23 +3,23 @@ import AddProduct from "@/components/addProduct";
 import AddProductButton from "@/components/addProductButton";
 import EditProduct from "@/components/EditProduct";
 import Product from "@/components/product";
+import Sort from "@/components/sort";
 import { useProductStore } from "@/stores/product-store";
 import { useUserStore } from "@/stores/user-store";
 
 const Page = () => {
     const isAddProductVisible = useProductStore((state) => state.addProductVisible);
     const editProductVisible = useProductStore((state) => state.editProductVisible);
-    const allProduct = useProductStore((state) => state.allProducts);
+    const allProduct = useProductStore((state) => state.displayedProducts);
     const userDetails = useUserStore((state) => state.user);
     const isOwner = useUserStore((state) => state.isOwner);
+
     return (
         <div className="mx-auto flex font-body flex-col pt-20 min-h-full ">
             {isAddProductVisible && <AddProduct />}
             {editProductVisible && <EditProduct />}
             <div className="h-10  items-center justify-end flex right-0 gap-2 px-1">
-                <div className="bg-amber-50 h-full w-20 rounded-lg flex items-center justify-center font-mono">
-                    Sort
-                </div>
+                <Sort />
                 <div className="bg-amber-50 h-full w-20 rounded-lg flex items-center justify-center font-mono">
                     Filter
                 </div>
