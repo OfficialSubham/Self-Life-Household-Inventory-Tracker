ad02e9bfbfbc8c689bc4983a99daa683cdde0f94
Subham Mondal <subhamoffi@gmail.com>
2026-05-25 20:18:11 +0530

    Created calculate badges count for all product

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { ProductDetails } from "./types";
 
 export const useHandleOnChange = <T extends Record<string, unknown>>(initialState: T) => {
     const [details, setDetails] = useState<T>(initialState);
@@ -13,4 +14,22 @@ export const useHandleOnChange = <T extends Record<string, unknown>>(initialStat
     };
 
     return { details, setDetails, handleOnChange };
+};
+
+export const calculateBadgesCountForAllProduct = (products: ProductDetails[]) => {
+    let freshItems = 0;
+    let expiringSoonItems = 0;
+    let expiredItems = 0;
+
+    for (const product of products) {
+        if (product.status == "fresh") freshItems++;
+        else if (product.status == "expired") expiredItems++;
+        else expiringSoonItems++;
+    }
+
+    return {
+        freshItems,
+        expiredItems,
+        expiringSoonItems,
+    };
 };
