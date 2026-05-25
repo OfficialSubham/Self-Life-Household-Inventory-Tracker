c82addc9ec5008534ff6cbc8a98fc3be7edbb277
Subham Mondal <subhamoffi@gmail.com>
2026-05-25 20:04:52 +0530

    Added user utils in user store

--- stores/user-store.ts
+++ stores/user-store.ts
@@ -2,21 +2,30 @@ import { create } from "zustand";
 
 type User = {
     email: string;
-    id: number;
+    _id: number;
     name: string;
+    householdId: number | null;
 };
 
 export type UserState = {
     user: User | null;
+    members: User[] | null;
+    isOwner: boolean;
 };
 
 export type UserActions = {
     setUser: (user: User) => void;
+    setMembers: (users: User[]) => void;
+    setIsOwner: (isOwner: boolean) => void;
 };
 
 export type UserStore = UserState & UserActions;
 
 export const useUserStore = create<UserStore>((set) => ({
     user: null,
+    members: null,
+    isOwner: false,
     setUser: (user) => set(() => ({ user })),
+    setMembers: (users) => set(() => ({ members: users })),
+    setIsOwner: (isOwner) => set(() => ({ isOwner })),
 }));
